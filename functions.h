fc9e1aa902a76e1e4f2e246215196793827f287c
Vladeslav515 <v.r.serytsya@student.csn.khai.edu>
2022-07-12 14:14:16 +0300

    Initial commit

---
+++ functions.h
@@ -0,0 +1,5 @@
+#ifndef FUNCTIONS
+#define FUNCTIONS
+
+
+#endif
---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "functions.h"
+
+int main(int argc, const char* argv[]){
+		
+
+
+	return 0;
+}
+
+
