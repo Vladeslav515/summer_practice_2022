a16d4e21c07c60feedc339afd9b78b0f1d1f51c6
Vladeslav515 <v.r.serytsya@student.csn.khai.edu>
2022-07-14 21:50:21 +0300

    works properly with 2 numeric system

--- main.c
+++ main.c
@@ -11,27 +11,31 @@
 #include "functions.h"
 
 int main(int argc, const char* argv[]){
-	int number = -120;
-	int memlength = 8;
-	int base = 2;
+	int number;
+	int memlength;
+	int base;
 	char string[25] = {0,};
 
 	printf("Enter a number: ");
 	scanf("%d", &number);
 
 	printf("Enter memory length: ");
-	scanf("%d", &number);
+	scanf("%d", &memlength);
 	
 	printf("Enter numeric number: ");
 	scanf("%d", &base);
-	
+
+	printf("\n");
+
+	toNumSystem(number, memlength, base, string);
+	printf("Number: %s\n", string);
+
 	if(number < 0){
-		toNumSystem(number, memlength, base, string);
-		printf("Number: %s\n", string);
 		Reversed_Code(string);
 		Addition_Code(string, memlength, base);
 	}
 	else{
+		printf("positive\n");
 		toNumSystem(number, memlength, base, string);
 		printf("Reversed number: %s\n", string);
 		printf("Addition code: %s\n", string);
